6df5aef61e764fa42aa5b69e86e431318dd33999
abrilsar <a.sar@correo.unimet.edu.ve>
2024-07-24 23:21:45 -0400

    input section

--- apps/backoffice/src/components/input/Input.tsx
+++ apps/backoffice/src/components/input/Input.tsx
@@ -12,7 +12,7 @@ export default function Input() {
       </label>
       <div className="relative flex w-full items-center">
         {/* <span className="absolute right-3 text-gray-500">🔍</span> */}
-        <QuestionMarkCircleIcon className="w-4 cursor-pointer text-blue-gray-500 items-center justify-center absolute right-3 text-gray-500" />
+        <QuestionMarkCircleIcon className="w-4 cursor-pointer text-blue-gray-500 items-center justify-center absolute right-3 text-gray-500 mr-0.5" />
         <input
           id="email-address"
           name="email"
@@ -22,6 +22,10 @@ export default function Input() {
           autoComplete="email"
           className="min-w-0 flex-auto rounded-md px-3.5 py-2.5 border border-gray-300 text-gray-700 shadow-sm  ring-inset focus:ring-2 focus:ring-inset focus:ring-buttonColor-100 sm:text-sm sm:leading-6"
         />
+        <p className="absolute mt-20 pt-2 text-gray-900 font-light text-sm items-start">
+          We care about your data in our{' '}
+          <span className="underline">privacy policy</span>.
+        </p>
       </div>
       {/* <input
         id="email-address"
